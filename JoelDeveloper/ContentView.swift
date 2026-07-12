20949dc30dbc390d573de4de14148797d3d8b70a
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-07-12 14:36:01 -0300

    Initial Commit

---
+++ JoelDeveloper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JoelDeveloper
+//
+//  Created by Joel de Almeida Souza on 12/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JoelDeveloper/JoelDeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoelDeveloperApp.swift
+//  JoelDeveloper
+//
+//  Created by Joel de Almeida Souza on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct JoelDeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
